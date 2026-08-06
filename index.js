995b19434ab62d9113e3f9c6518b3cc019f7c338
raja <konig.9122005@gmail.com>
2026-08-06 11:51:11 +0530

    updated to v3

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    return res.status(200).json({ message: "Hello from the server v2" });
+    return res.status(200).json({ message: "Hello from the server v3" });
 })
 app.get("/health", (req, res) => {
     return res.status(200).json({ message: "Health is fine!!" });
 })
-
 app.listen(PORT, () => {
     console.log(`listening on PORT ${PORT}`)
 })
