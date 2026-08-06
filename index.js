c1c4aaa80a322c7d6d95b57002791237b03710f4
raja <konig.9122005@gmail.com>
2026-08-06 10:46:10 +0530

    updated server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-    return res.status(200).json({ message: "Hello from the server" });
+    return res.status(200).json({ message: "Hello from the server v2" });
 })
 app.get("/health", (req, res) => {
     return res.status(200).json({ message: "Health is fine!!" });
