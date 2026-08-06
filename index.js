17dd81f6e9b947d839e71264a5de5a8f251c3638
raja <konig.9122005@gmail.com>
2026-08-06 09:52:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    return res.status(200).json({ message: "Hello from the server" });
+})
+app.get("/health", (req, res) => {
+    return res.status(200).json({ message: "Health is fine!!" });
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on PORT ${PORT}`)
+})
